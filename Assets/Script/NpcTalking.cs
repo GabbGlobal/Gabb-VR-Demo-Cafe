95e4556cbc0ad937672f43d905deab554bf263d0
unknown <sidchou93@gmail.com>
2022-10-29 10:15:50 -0400

    add anim

--- Assets/Script/NpcTalking.cs
+++ Assets/Script/NpcTalking.cs
@@ -7,22 +7,17 @@ public class NpcTalking : MonoBehaviour
     
     private GameObject player;
     private bool talking = false;
-<<<<<<< HEAD
     private Animator animator;
-=======
->>>>>>> d09b87c59bf04bd1f85cad2f5d91e3edf26899f9
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectsWithTag("MainCamera")[0];
-<<<<<<< HEAD
         animator = GetComponent<Animator>();
         if (animator == null)
         {
             Debug.LogError("animator is null");
         }
-=======
->>>>>>> d09b87c59bf04bd1f85cad2f5d91e3edf26899f9
+
     }
 
     // Update is called once per frame
@@ -37,13 +32,12 @@ public class NpcTalking : MonoBehaviour
     public void Talk(bool _talk)
     {
         talking = _talk;
-<<<<<<< HEAD
+
         if (talking)
         {
             animator.SetInteger("talking", Random.Range(0, 1));
             animator.SetTrigger("Talk");
         }
-=======
->>>>>>> d09b87c59bf04bd1f85cad2f5d91e3edf26899f9
+
     }
 }
